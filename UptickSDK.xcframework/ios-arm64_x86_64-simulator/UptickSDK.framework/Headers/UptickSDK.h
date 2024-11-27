bbd948997f619c1fd56021435169cf8aec58b568
Nathan Palmer <nathanpalmer@users.noreply.github.com>
2024-11-27 18:25:32 -0500

    build: Remove xcframework

--- UptickSDK.xcframework/ios-arm64_x86_64-simulator/UptickSDK.framework/Headers/UptickSDK.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-
-//! Project version number for UptickSDK.
-FOUNDATION_EXPORT double UptickSDKVersionNumber;
-
-//! Project version string for UptickSDK.
-FOUNDATION_EXPORT const unsigned char UptickSDKVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <UptickSDK/PublicHeader.h>
